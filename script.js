f6803f3f418c13ab81b8b92a1b212e5bd228f7e6
Ronith Reddy <ronithreddy2205@gmail.com>
2025-11-12 07:18:48 +0530

    Add password generation function

---
+++ script.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  let password = "";
+  const array = new Uint32Array(length);
+  crypto.getRandomValues(array);
+  for (let i = 0; i < length; i++) {
+    password += charset[array[i] % charset.length];
+  }
+  document.getElementById("result").value = password;
+}
